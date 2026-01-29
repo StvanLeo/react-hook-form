49bb4f3782cb5651dfa80e4089170973ab20dd81
StvanLeo <estebanoleas@gmail.com>
2026-01-29 08:10:29 -0500

    Add the contact form to add more validations such as minLength and pattern

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
-import Form from './components/Form'
+import ContactForm from './components/ContactForm'
+// import Form from './components/Form'
 
 function App() {
 
   return (
     <>
-      <Form />
+      {/*<Form />*/}
+      <ContactForm />
     </>
   )
 }
---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,47 @@
+import { useForm } from "react-hook-form";
+
+const ContactForm = () => {
+    const { register, handleSubmit, formState: { errors }  } = useForm();
+    const onSubmit = data => {
+        console.log(data);
+    };
+
+    return (
+        <>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div>
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" 
+                        {...register("name", {required: "This field is required"})} />
+                    <br/>
+                    { errors.name && <span>{errors.name.message}</span>}
+                </div>
+                <div>
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" {...register("email", {
+                        required: "This field is required", 
+                        pattern: { value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, 
+                        message: "The email format is invalid."}})} />
+                    <br/>
+                    { errors.email && <span>{errors.email.message}</span>}
+                </div>
+                <div>
+                    <label htmlFor="message">Message:</label>
+                    <textarea id="message"
+                        {...register("message", {required: "This field is required", 
+                            minLength: {value: 10, message: "The message must be at least 20 characters long"}
+                        })}>
+                    </textarea>
+                    <br/>
+                    { errors.message && <span>{errors.message.message}</span>}
+                    
+                </div>
+                <div>
+                    <button type="submit">Send</button>
+                </div>
+            </form>
+        </>
+    )
+};
+
+export default ContactForm;
